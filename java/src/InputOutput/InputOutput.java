1782778285edacb8fc80ddfe12dd591c33b99ed1
sumitsidana <sidana.sumit@gmail.com>
2018-09-09 01:01:04 +0200

    write only positive ratings

--- java/src/InputOutput/InputOutput.java
+++ java/src/InputOutput/InputOutput.java
@@ -7,25 +7,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class InputOutput {
-	public static void writeTrainTestForDiversityMetrics(String inputFile, String outputFile) throws IOException{
+	public static void writePosRatings(String inputFile, String outputFile) throws IOException{
 
 		PrintWriter printWriter = new PrintWriter (outputFile);
 		try (BufferedReader br = new BufferedReader(new FileReader(new File(inputFile)))) {
 			String line;
-			while ((line = br.readLine()) != null) {
-				//				int lastIndexofTab = line.lastIndexOf("\t");
-				int lastIndexofTab = line.lastIndexOf(",");
-				String [] array = line.split(",");
-				//				if((line.substring(lastIndexofTab+1)).equals("1")){
-				if(array[2].equals("1")){
-					//					printWriter.println((line.substring(0, lastIndexofTab))+"\t"+"0.0");
-					printWriter.println(array[0]+"\t"+array[1]+"\t"+"0.0");
+			while ((line = br.readLine()) != null) {				
+				String [] array = line.split("\t");
+				//				
+				if(array[2].equals("4")){
+					printWriter.println(line);
 				}
-				else{
-					printWriter.println(array[0]+"\t"+array[1]+"\t"+"1.0");
-				}
-
-
 
 				// TODO Auto-generated method stub
 			}
