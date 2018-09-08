0211102e9c739a60ba93ddfe0688063e08168112
sumitsidana <sidana.sumit@gmail.com>
2018-09-08 22:11:51 +0200

    right version packages

--- MLP.py
+++ MLP.py
@@ -12,7 +12,7 @@ import theano
 import theano.tensor as T
 import keras
 from keras import backend as K
-from keras import initializations
+from keras import initializers
 from keras.regularizers import l2, activity_l2
 from keras.models import Sequential, Graph, Model
 from keras.layers.core import Dense, Lambda, Activation
