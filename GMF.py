11267fa1388aa8b20d80d30111d8cb09a51f28f7
sumitsidana <sidana.sumit@gmail.com>
2018-09-08 22:47:07 +0200

    roll back to previous versions

--- GMF.py
+++ GMF.py
@@ -10,11 +10,10 @@ import numpy as np
 import theano.tensor as T
 import keras
 from keras import backend as K
-from keras import initializers
+from keras import initializations
 from keras.models import Sequential, Model, load_model, save_model
 from keras.layers.core import Dense, Lambda, Activation
-from keras.layers import Embedding, Input, Dense, merge, Reshape, Flatten
-#  Merge
+from keras.layers import Embedding, Input, Dense, merge, Reshape, Merge, Flatten
 from keras.optimizers import Adagrad, Adam, SGD, RMSprop
 from keras.regularizers import l2
 from Dataset import Dataset
