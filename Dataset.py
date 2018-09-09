1a90594f4f6ae310f647488c62fb7de649c733a3
sumitsidana <sidana.sumit@gmail.com>
2018-09-09 16:43:25 +0200

    add proper file names

--- Dataset.py
+++ Dataset.py
@@ -16,8 +16,7 @@ class Dataset(object):
         '''
         Constructor
         '''
-        #self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
-        self.trainMatrix = self.load_rating_file_as_matrix(path)
+        self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
         self.testRatings = self.load_rating_file_as_list(path + ".test.rating")
         self.testNegatives = self.load_negative_file(path + ".test.negative")
         assert len(self.testRatings) == len(self.testNegatives)
